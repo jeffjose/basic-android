318004cf4300d844b1cb3bcd012bf8b4b1a4017a
Jeffrey Jose <jeffjosejeff@gmail.com>
2024-08-16 00:05:43 -0700

    code builds

--- .canonical-app/app/src/main/java/com/example/cupcake/ui/ThirdScreen.kt
+++ .canonical-app/app/src/main/java/com/example/cupcake/ui/ThirdScreen.kt
@@ -11,10 +11,13 @@ import androidx.compose.ui.res.*
 import androidx.compose.ui.tooling.preview.*
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.*
 import androidx.navigation.compose.*
 import com.example.cupcake.R
 import com.example.cupcake.ui.theme.*
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 import retrofit2.Call
 import retrofit2.Callback
@@ -25,6 +28,8 @@ import retrofit2.http.GET
 
 @Composable
 fun ThirdScreen(navController: NavHostController, modifier: Modifier = Modifier) {
+
+    val viewModel: ViewModel2 = viewModel()
 
     Column(modifier = modifier, verticalArrangement = Arrangement.SpaceBetween) {
         Column(
@@ -67,29 +72,29 @@ fun ThirdScreenPreview() {
 
 data class Todo(var userId: Int, var id: Int, var title: String, var completed: Boolean)
 
-const val BASE_URL = "https://jsonplaceholder.typicode.com/"
-
-interface APIService {
-    @GET("todos") suspend fun getTodos(): List<Todo>
-
-    companion object {
-        var apiService: APIService? = null
-        fun getInstance(): APIService {
-            if (apiService == null) {
-                apiService =
-                        Retrofit.Builder()
-                                .baseUrl(BASE_URL)
-                                .addConverterFactory(GsonConverterFactory.create())
-                                .build()
-                                .create(APIService::class.java)
-            }
-            return apiService!!
-        }
-    }
-}
+// const val BASE_URL = "https://jsonplaceholder.typicode.com/"
+//
+// interface APIService {
+//    @GET("todos") suspend fun getTodos(): Todo
+//
+//    companion object {
+//        var apiService: APIService? = null
+//        fun getInstance(): APIService {
+//            if (apiService == null) {
+//                apiService =
+//                        Retrofit.Builder()
+//                                .baseUrl(BASE_URL)
+//                                .addConverterFactory(GsonConverterFactory.create())
+//                                .build()
+//                                .create(APIService::class.java)
+//            }
+//            return apiService!!
+//        }
+//    }
+// }
 
 interface ApiService2 {
-    @GET("todos") fun getTodos(): Call<Todo>
+    @GET("todos") fun getTodos(): Call<List<Todo>>
 }
 
 object RetrofitInstance {
@@ -104,48 +109,58 @@ object RetrofitInstance {
     val apiService: ApiService2 by lazy { retrofit.create(ApiService2::class.java) }
 }
 
-class TodoViewModel : ViewModel() {
-    private val _todoList = mutableStateListOf<Todo>()
-    var errorMessage: String by mutableStateOf("")
-    val todoList: List<Todo>
-        get() = _todoList
-
-    fun getTodoList() {
-        viewModelScope.launch {
-            val apiService = APIService.getInstance()
-            try {
-                _todoList.clear()
-                _todoList.addAll(apiService.getTodos())
-            } catch (e: Exception) {
-                errorMessage = e.message.toString()
-            }
-        }
-    }
-}
+// class TodoViewModel : ViewModel() {
+//    private val _todoList = mutableStateListOf<Todo>()
+//    var errorMessage: String by mutableStateOf("")
+//    val todoList: List<Todo>
+//        get() = _todoList
+//
+//    fun getTodoList() {
+//        viewModelScope.launch {
+//            val apiService = APIService.getInstance()
+//            try {
+//                _todoList.clear()
+//                _todoList.addAll(apiService.getTodos())
+//            } catch (e: Exception) {
+//                errorMessage = e.message.toString()
+//            }
+//        }
+//    }
+// }
 
 class ViewModel2 : ViewModel() {
     var loading: Boolean by mutableStateOf(false)
+    private val _todos: MutableStateFlow<List<Todo>> = MutableStateFlow(listOf())
+    val todos: StateFlow<List<Todo>> = _todos
 
     init {
-        retrieveAgentsData()
+        getTodosData()
     }
 
-    private fun retrieveAgentsData() {
+    private fun getTodosData() {
         viewModelScope.launch {
             loading = true
-            val call: Call<Todo> = RetrofitInstance.apiService.getTodos()
+            val call: Call<List<Todo>> = RetrofitInstance.apiService.getTodos()
             call.enqueue(
-                    object : Callback<Todo> {
-                        override fun onResponse(call: Call<Todo>, response: Response<Todo>) {
+                    object : Callback<List<Todo>> {
+                        override fun onResponse(
+                                call: Call<List<Todo>>,
+                                response: Response<List<Todo>>
+                        ) {
                             if (response.isSuccessful) {
-                                // TODO: set data here
+                                // val responseData: List<Todo>? = response.body()?.data
+                                val responseData: List<Todo>? = response.body()
+                                if (responseData != null) {
+                                    _todos.value = responseData
+                                }
+
                                 loading = false
                             } else {
                                 loading = false
                             }
                         }
 
-                        override fun onFailure(call: Call<Todo>, t: Throwable) {
+                        override fun onFailure(call: Call<List<Todo>>, t: Throwable) {
                             loading = false
                         }
                     }
