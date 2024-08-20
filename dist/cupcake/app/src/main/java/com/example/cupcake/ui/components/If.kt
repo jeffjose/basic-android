9bd84a0166563b9ac03eebbdeae6a0d138ce7ec0
Jeffrey Jose <jeffjosejeff@gmail.com>
2024-08-20 12:50:59 -0700

    if

--- dist/cupcake/app/src/main/java/com/example/cupcake/ui/components/If.kt
+++ dist/cupcake/app/src/main/java/com/example/cupcake/ui/components/If.kt
@@ -6,25 +6,33 @@ import com.example.cupcake.ui.theme.CupcakeTheme
 
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 
 @Composable
 fun If() {
     
 
-val user = mutableMapOf(
-  "loggedIn" to false
-)
+var count by remember { mutableStateOf(0) }
+val user by remember { mutableStateOf(mutableMapOf( "loggedIn" to false)) }
 println(user)
 
-Button(onClick={ user["loggedIn" ] = true }){
+Button(onClick={ count-- }){
   Text(text="Log in ")
 }
 
 
-Button(onClick={ user["loggedIn" ] = false }){
+Button(onClick={ 
+  println("hi $user") 
+  user["loggedIn"] = true
+  println("bye $user") 
+  }){
   Text(text="Log out ")
 }
 
+Text(text="$count")
 Text(text="$user")
 
 }
