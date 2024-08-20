474fbebea488a31faa6b6e43317ca4faa163f2f2
Jeffrey Jose <jeffjosejeff@gmail.com>
2024-08-20 15:36:49 -0700

    WORKS: dataclass & frontmatter

--- dist/cupcake/app/src/main/java/com/example/cupcake/ui/components/If.kt
+++ dist/cupcake/app/src/main/java/com/example/cupcake/ui/components/If.kt
@@ -1,76 +1,61 @@
 package com.example.cupcake.ui.components
 
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.cupcake.ui.theme.CupcakeTheme
-
 import android.os.Parcelable
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateMapOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import kotlinx.parcelize.Parcelize
 
-
-
 @Parcelize
-data class User(val loggedIn: Boolean) : Parcelable
-
+data class User(
+    var loggedIn: Boolean,
+) : Parcelable
 
 @Composable
 fun If() {
-    
+// println("if - before")
 
-//println("if - before")
+    var foo = 0
+    var count by remember { mutableStateOf(0) }
+    var user by rememberSaveable { mutableStateOf(User(false)) }
 
+// println("if - after ${user["loggedIn"]}")
 
-var foo = 0
-var count by remember { mutableStateOf(0) }
-val user1 by rememberSaveable { mutableStateOf(User(false)) }
-val user = mutableStateMapOf<String,Boolean>("loggedIn" to false)
+    if (user.loggedIn == false) {
+        Button(onClick = {
+            user = user.copy(loggedIn = true)
+            println("login! ${user.loggedIn}")
+            foo++
+        }) {
+            Text(text = "Log in ")
+        }
+    } else {
+        Button(onClick = {
+            user = user.copy(loggedIn = false)
+            println("logout! ${user.loggedIn}")
+            foo++
+        }) {
+            Text(text = "Log out ")
+        }
+    }
 
-//println("if - after ${user["loggedIn"]}")
+    Button(onClick = {
+        if (user.loggedIn == true) {
+            user = user.copy(loggedIn = false)
+        } else {
+            user = user.copy(loggedIn = true)
+        }
+        foo++
+    }) {
+        Text(text = "Toggle")
+    }
 
-if (user["loggedIn"] == false) {
-  Button(onClick={ 
-    user["loggedIn"] = true
-    println("login! ${user["loggedIn"]}")
-    foo++
-    }){
-    Text(text="Log in ")
-  }
-}
-else {
-
-  Button(onClick={ 
-    user["loggedIn"] = false
-    println("logout! ${user["loggedIn"]}")
-    foo++
-    }){
-    Text(text="Log out ")
-  }
-}
-
-Button(onClick={ 
-  if (user["loggedIn"] == true) {
-
-user["loggedIn"] = false
-  }
-  else {
-user["loggedIn"] = true
-
-  }
-    foo++
-  }){
-  Text(text="Toggle")
-}
-
-  Text(text="${user.getValue("loggedIn")}. Clicked $foo times")
-
+    Text(text = "${user.loggedIn}. Clicked $foo times")
 }
 
 /*
@@ -82,4 +67,4 @@ fun IfPreview() {
         )
     }
     }
-    */
+ */
