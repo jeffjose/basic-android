05b6cafb7c9c0f529f7371e5ead23daeaf3365d6
Jeffrey Jose <jeffjosejeff@gmail.com>
2024-08-20 17:33:01 -0700

    work in progress - keypad 3

--- dist/cupcake/app/src/main/java/com/example/cupcake/ui/components/Keypad.kt
+++ dist/cupcake/app/src/main/java/com/example/cupcake/ui/components/Keypad.kt
@@ -4,6 +4,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.cupcake.ui.theme.CupcakeTheme
 
+import androidx.compose.foundation.layout.Row
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 
@@ -14,33 +15,35 @@ fun Keypad(value) {
     
 
 
+Text(text=value)
+
 Row() {
-  Button(){Text(text="1")}
-  Button(){Text(text="2")}
-  Button(){Text(text="3")}
+  Button(onClick={}){Text(text="1")}
+  Button(onClick={}){Text(text="2")}
+  Button(onClick={}){Text(text="3")}
 
 }
 
 
 Row() {
-  Button(){Text(text="4")}
-  Button(){Text(text="5")}
-  Button(){Text(text="6")}
+  Button(onClick={}){Text(text="4")}
+  Button(onClick={}){Text(text="5")}
+  Button(onClick={}){Text(text="6")}
 
 }
 
 
 Row() {
-  Button(){Text(text="7")}
-  Button(){Text(text="8")}
-  Button(){Text(text="9")}
+  Button(onClick={}){Text(text="7")}
+  Button(onClick={}){Text(text="8")}
+  Button(onClick={}){Text(text="9")}
 }
 
 
 Row() {
-  Button(){Text(text="Clear")}
-  Button(){Text(text="0")}
-  Button(){Text(text="Submit")}
+  Button(onClick={}){Text(text="Clear")}
+  Button(onClick={}){Text(text="0")}
+  Button(onClick={}){Text(text="Submit")}
 }
 
 }
